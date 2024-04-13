91674dcd982c45317d54f5c4176a21f0b08880df
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-04-13 16:08:42 +0200

    add timer for shoot

--- player.gd
+++ player.gd
@@ -5,10 +5,17 @@ const SPEED = 100.0
 const bulletPath = preload("res://bullet.tscn")
 @onready var animation = $AnimatedSprite2D
 
+func _ready():
+	$Timer.start()
+	
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		shoot()
 	$Node2D.look_at(get_global_mouse_position())
+	print($Timer.time_left)
+	if $Timer.is_stopped():
+		shoot()
+		$Timer.start()
 	
 func _physics_process(delta):
 	var mouseOffset = get_viewport().get_mouse_position() - self.position;
@@ -29,7 +36,6 @@ func shoot():
 	var bullet = bulletPath.instantiate()
 	get_parent().add_child(bullet)
 	bullet.position = $Node2D/Marker2D.global_position
-	print(get_global_mouse_position().x)
 	if animation.flip_h == false:
 		bullet.velocity.x = 1
 	else:
