aba2ae1f8afa77d13d470fe56e151344803c3536
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-04-13 12:56:13 +0200

    add run animation

--- Player.gd
+++ Player.gd
@@ -4,14 +4,17 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta):
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		animated_sprite.play("run")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.stop()
 
 	move_and_slide()
