06dd3bd5ba449a9e2f8ca7735de24b2eeec53f39
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-04-13 13:30:12 +0200

    move player with mouse

--- Player.gd
+++ Player.gd
@@ -1,20 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+const SPEED = 100.0
+var mouse_position = null
 @onready var animated_sprite = $AnimatedSprite2D
 
-
 func _physics_process(delta):
-
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		animated_sprite.play("run")
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite.stop()
-
+	mouse_position = get_global_mouse_position()
+	
+	var direction = (mouse_position - position)
+	velocity = direction * delta * SPEED
+	#if direction:
+		##if direction == -1:
+			##animated_sprite.flip_h = true
+		##else:
+			##animated_sprite.flip_h = false
+		#animated_sprite.play("run")
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#animated_sprite.play("idle")
 	move_and_slide()
