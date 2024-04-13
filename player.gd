aaa55b2adba0a3ec15862eff3bd8aba6e9170947
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-04-13 15:31:22 +0200

    add shoot without direction

--- player.gd
+++ player.gd
@@ -2,9 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
+const bulletPath = preload("res://bullet.tscn")
 @onready var animation = $AnimatedSprite2D
 
-
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		shoot()
+	
 func _physics_process(delta):
 	var mouseOffset = get_viewport().get_mouse_position() - self.position;
 	var direction = mouseOffset.normalized() * SPEED
@@ -19,3 +23,8 @@ func _physics_process(delta):
 		animation.play("run")
 	velocity = direction * delta * SPEED
 	move_and_slide()
+	
+func shoot():
+	var bullet = bulletPath.instantiate()
+	get_parent().add_child(bullet)
+	bullet.position = $Marker2D.global_position
