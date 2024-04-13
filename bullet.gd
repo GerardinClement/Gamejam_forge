b0389c019064cf53f503c424b2c76ca24a45840d
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-04-13 19:45:14 +0200

    remove the bullet when it hits an enemy

--- bullet.gd
+++ bullet.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
+
 @onready var animated_sprite = $AnimatedSprite2D
 func _physics_process(delta):
 	animated_sprite.play("default")
 	move_and_collide(velocity.normalized() * delta * SPEED)
+
+func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	if area.name == "enemy":
+		area.get_parent().queue_free()
+	self.queue_free()
