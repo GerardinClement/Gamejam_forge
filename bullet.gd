d42a6fcec809ea5645fbbb0edf2b39713ac06cd0
Clement Gerardin <clementgerardin.cg@gmail.com>
2024-04-13 15:47:43 +0200

    add direction

--- bullet.gd
+++ bullet.gd
@@ -3,5 +3,4 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 func _physics_process(delta):
-	velocity.x = 2;
 	move_and_collide(velocity.normalized() * delta * SPEED)
--- player.gd
+++ player.gd
@@ -8,6 +8,7 @@ const bulletPath = preload("res://bullet.tscn")
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		shoot()
+	$Node2D.look_at(get_global_mouse_position())
 	
 func _physics_process(delta):
 	var mouseOffset = get_viewport().get_mouse_position() - self.position;
@@ -27,4 +28,9 @@ func _physics_process(delta):
 func shoot():
 	var bullet = bulletPath.instantiate()
 	get_parent().add_child(bullet)
-	bullet.position = $Marker2D.global_position
+	bullet.position = $Node2D/Marker2D.global_position
+	print(get_global_mouse_position().x)
+	if animation.flip_h == false:
+		bullet.velocity.x = 1
+	else:
+		bullet.velocity.x = -1
