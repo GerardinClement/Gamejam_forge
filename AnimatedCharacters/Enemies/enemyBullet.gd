9d659a071c3ac356cf4669d0e100a746584de757
mthibaul <maxime.thibault21490@gmail.com>
2024-04-13 21:28:44 +0200

    Enemies attack the hero

--- AnimatedCharacters/Enemies/enemyBullet.gd
+++ AnimatedCharacters/Enemies/enemyBullet.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-@onready var player = get_parent().get_parent().get_node("Player")
-const speed = 3000
-
+#@onready var player = get_parent().get_parent().get_node("Player")
+const speed = 300.0
+@onready var player = get_parent().get_node("Player")
 
 func _ready():
 	pass # Replace with function body.
@@ -10,5 +10,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	velocity = global_position.direction_to(player.global_position)
-	move_and_collide(velocity * speed * delta)
+	if $AnimatedSprite2D:
+		$AnimatedSprite2D.play("default")
+	if velocity == Vector2(0, 0):
+		velocity = position.direction_to(player.global_position)
+		look_at(player.global_position)
+	move_and_collide(velocity.normalized() * speed * delta)
--- ennemies.gd
+++ ennemies.gd
@@ -1,32 +1,65 @@
 extends CharacterBody2D
 
 
-@export var speed = 100
+@export var speed = 60
 @onready var player = get_parent().get_node("Player")
+var hasShot = true
+var animations
 var playerPos
 var enemyPos
 var bulletPath
 
+func _ready():
+	animations = get_node("animations")
+
+func _process(delta):
+	if $Timer.is_stopped():
+		hasShot = false
+		if enemyPos.x < playerPos.x:
+			animations.play("shootRight")
+		else:
+			animations.play("shootLeft")
+		$Timer.set_wait_time(1)
+		$Timer.start()
+		
+	if checkFrame() && !hasShot:
+		shoot()
+		hasShot = true
+
 func _physics_process(delta):
-	var animations = get_node("animations")
+	if animations.is_playing() && (animations.animation != "moveRight" || animations.amination != "moveLeft"):
+		return
 	playerPos = player.position
 	enemyPos = (playerPos - position).normalized()
 	velocity = global_position.direction_to(player.global_position)
-	if enemyPos.x > 0:
+	if enemyPos.x > 0 && !animations.is_playing():
 		animations.play("moveRight")
-	else:
+	elif !animations.is_playing():
 		animations.play("moveLeft")
 	move_and_collide(velocity * speed * delta)
-	shoot()
+
 
 func shoot():
 	getBulletPath()
-	print(bulletPath)
 	if bulletPath:
 		var bullet = bulletPath.instantiate()
-		add_child(bullet)
+		get_parent().add_child(bullet)
+		print(get_node("gunPos").global_position)
+		bullet.position = get_node("gunPos").global_position
 
 func getBulletPath():
 	if get_name() == 'octopus':
 		bulletPath = load("res://AnimatedCharacters/Enemies/octopus/Bullet.tscn")
+	elif get_name() == 'mech':
+		bulletPath = load("res://AnimatedCharacters/Enemies/mech/Bullet.tscn")
+	elif get_name() == 'human':
+		bulletPath = load("res://AnimatedCharacters/Enemies/human/Bullet.tscn")
 		
+func checkFrame():
+	if get_name() == 'octopus' && animations.frame == 2:
+		return true
+	elif get_name() == 'mech' && animations.frame == 3:
+		return true
+	elif get_name() == 'human' && animations.frame == 1:
+		return true
+	return false
