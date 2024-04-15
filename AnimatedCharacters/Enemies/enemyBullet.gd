984e6dfbc7abbea0a3dd23d9edbaae0fc8c18060
mthibaul <maxime.thibault21490@gmail.com>
2024-04-16 00:42:37 +0200

    Added animation when bullets hit objects

--- AnimatedCharacters/Enemies/enemyBullet.gd
+++ AnimatedCharacters/Enemies/enemyBullet.gd
@@ -5,7 +5,7 @@ extends Area2D
 var speed
 var damage
 var bulletPath
-var isReady = false
+var isShot = false
 var velocity = Vector2()
 @onready var player = get_parent().get_node("Player")
 
@@ -14,11 +14,11 @@ func _ready():
 
 func _physics_process(delta):
 	if bulletPath:
-		var bulletSprite = bulletPath.instantiate()
-		add_child(bulletSprite)
-		bulletSprite.get_node("AnimatedSprite2D").play("default")
-		if !isReady:
+		if !isShot:
+			var bulletSprite = bulletPath.instantiate()
+			add_child(bulletSprite)
+			bulletSprite.get_node("AnimatedSprite2D").play("default")
 			look_at(player.global_position)
 			velocity = bulletSprite.global_position.direction_to(player.global_position)
-			isReady = true
+			isShot = true
 		global_position += velocity * speed * delta
