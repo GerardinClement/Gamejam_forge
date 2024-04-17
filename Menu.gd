4df486333a4104380b6d22d9963ac2453b708a03
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-04-17 16:38:05 +0200

    add blue menu

--- Menu.gd
+++ Menu.gd
@@ -1,14 +1,13 @@
 extends Control
 
-@onready var animation = $AnimatedSprite2D
+@onready var animation_mecha = $Mecha
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animation.flip_h = true
-	animation.play("mecha_animation")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	animation_mecha.play("mecha_animation")
+	animation_mecha.flip_h = true
+	
+# Called every frame. 'delta' is animation_mechathe elapsed time since the previous frame.
 func _process(delta):
 	pass
 
