078423e6f94477e003e23c05a0560d6d5f385ec9
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-04-18 16:27:55 +0200

    game over screen done

---
+++ GameOver.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var animation_mecha = $Mecha
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation_mecha.play("mecha_shoot")
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_back_to_menu_button_pressed():
+	get_tree().change_scene_to_file("res://Menu.tscn")
+
+
+func _on_restart_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
