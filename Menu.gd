457d92ff3a1b761f7995c2985f1148a7fc4c1d8b
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2024-04-18 14:53:51 +0200

    add death animation when pressing start | add world map

--- Menu.gd
+++ Menu.gd
@@ -1,11 +1,17 @@
 extends Control
 
 @onready var animation_mecha = $Mecha
+@onready var animation_background = $Background
+@onready var animation_mecha_death = $MechaDeath
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_mecha.play("mecha_animation")
 	animation_mecha.flip_h = true
+	animation_background.play("background_animation")
+	animation_background.modulate.a = 0.5
+	#$StartButton.connect("pressed", Callable(self, "_on_start_button_pressed"))
+	$Timer.connect("timeout", Callable(self, "_on_Timer_timeout"))
 	
 # Called every frame. 'delta' is animation_mechathe elapsed time since the previous frame.
 func _process(delta):
@@ -13,8 +19,25 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+	animation_mecha.modulate.a = 0
+	animation_mecha_death.play("mecha_death")
+	animation_mecha_death.flip_h = true
+	$Timer.start()
 
+func _on_Timer_timeout():
+	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+# Assuming you have an AnimationPlayer node named "AnimationPlayer"
+# and a Button node named "StartButton"
+#
+#func _ready():
+	## Connect the button's pressed signal to a function
+	#
+#
+#func _on_StartButton_pressed():
+	## Play the new animation when the start button is pressed
+	#$AnimationPlayer.play("NewAnimationName")
