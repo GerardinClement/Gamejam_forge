5370acaa56af2d1a100749608460bf52a7c7c89b
boulatrousse <lboulatrous@gmail.com>
2024-04-19 17:55:10 +0200

    add backToMenu button in escape interface

--- BackToMenuButton.gd
+++ BackToMenuButton.gd
@@ -9,11 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-
-func _on_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
-
-
-func _on_back_to_menu_button_pressed():
-	get_tree().change_scene_to_file("res://Menu.tscn")
