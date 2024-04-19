f5a2ad8891ab33ab7e11582dc75ea78621d0bf6e
mthibaul <maxime.thibault21490@gmail.com>
2024-04-20 00:17:28 +0200

    Fixed missing files

--- script/player.gd
+++ script/player.gd
@@ -209,7 +209,8 @@ func _on_animated_sprite_2d_animation_finished():
 func add_scent():
 	var scent = scent_scene.instantiate()
 	scent.player = player
-	scent.position = self.position
+	scent.position = position
+	print(scent.position)
 	
 	get_parent().add_child(scent)
 	player.scent_trail.push_front(scent)
