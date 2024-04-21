e4a0fad9ed1bc04746ae18e68e95304933899337
boulatrousse <lboulatrous@gmail.com>
2024-04-21 11:56:41 +0200

    add sound for the key

--- script/Key.gd
+++ script/Key.gd
@@ -5,6 +5,7 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_key.play("key_anim")
+	$Timer.connect("timeout", Callable(self, "_on_Timer_timeout"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,5 +14,10 @@ func _process(delta):
 
 
 func _on_body_entered(body):
+	$KeySound.play()
+	$KeyAnimation.modulate.a = 0
+	$Timer.start()
+	
+func _on_Timer_timeout():
 	Global.playerKeys += 1
 	queue_free()
