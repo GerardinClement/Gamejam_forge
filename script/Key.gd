918b4b4e7345e93b1d669a663dd0eb9203f31b6f
boulatrousse <lboulatrous@gmail.com>
2024-04-21 11:14:09 +0200

    add key scene and script

---
+++ script/Key.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var animation_key = $KeyAnimation
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation_key.play("key_anim")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	Global.playerKeys += 1
+	animation_key.modulate.a = 0
